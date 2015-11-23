94beee928290b146da2d4c69bdea80bad6e8990c
sudha <sudha>
2015-11-23 13:55:34 +0530

    changed config

--- phpmyadmin4.1.14/config.inc.php
+++ phpmyadmin4.1.14/config.inc.php
@@ -1,52 +1,14 @@
-<?php
-/* vim: set expandtab sw=4 ts=4 sts=4: */
-/**
- * phpMyAdmin sample configuration, you can use it as base for
- * manual configuration. For easier setup you can use setup/
- *
- * All directives are explained in documentation in the doc/ folder
- * or at <http://docs.phpmyadmin.net/>.
- *
- * @package PhpMyAdmin
- */
-
-/*
- * This is needed for cookie based authentication to encrypt password in
- * cookie
- */
-$cfg['blowfish_secret'] = 'a8b7c6d'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
-
-/*
- * Servers configuration
- */
-$i = 0;
-
-/*
- * First server
- */
-$i++;
-/* Authentication type */
-$cfg['Servers'][$i]['verbose'] = 'mysql wampserver';
-//$cfg['Servers'][$i]['auth_type'] = 'cookie';
-$cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['user'] = 'root';
-$cfg['Servers'][$i]['password'] = '';
-/* Server parameters */
-$cfg['Servers'][$i]['host'] = '127.0.0.1';
-$cfg['Servers'][$i]['connect_type'] = 'tcp';
-$cfg['Servers'][$i]['compress'] = false;
-/* Select mysql if your server does not have mysqli */
-$cfg['Servers'][$i]['extension'] = 'mysqli';
-$cfg['Servers'][$i]['AllowNoPassword'] = true;
-
-
-
-/*
- * phpMyAdmin configuration storage settings.
- */
- 
-// No warning on  pmadb tables
-$cfg['PmaNoRelation_DisableWarning'] = true;
-
-
-?>
+<?php  
+$cfg['blowfish_secret'] = 'ba17c1ec07d65003';  // use here a value of your choice  
+$i = 0;  
+/* First server */  
+$i++;  
+/* Authentication type */  
+$cfg['Servers'][$i]['auth_type'] = 'cookie';  
+/* Server parameters */  
+$cfg['Servers'][$i]['host'] = 'ap-cdbr-azure-southeast-a.cloudapp.net';  // Replace with value from connection string  
+$cfg['Servers'][$i]['connect_type'] = 'tcp';  
+$cfg['Servers'][$i]['compress'] = false;  
+$cfg['Servers'][$i]['extension'] = 'mysqli';  
+$cfg['Servers'][$i]['AllowNoPassword'] = false;  
+?>  
